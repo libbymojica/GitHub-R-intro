d1e5d6a78d4b51eb562a6117d5545a24c0c4c8ac
libbymojica <Libby.Mojica@colostate.edu>
2023-01-23 17:42:38 -0700

    Final week 1 exercises submission

---
+++ tips and tricks.R
@@ -0,0 +1,32 @@
+# tips and tricks for R
+
+#when close session, R asks if you want to save
+#saves variables from environment
+# tools, global options, change save setting never
+
+# R markdown setting
+# see code in line - change in tools - global options = r markdown
+
+#use rainbow paretheses
+test(x9x(x99x(((xx((())))))))
+
+#visual editor for r markdown document
+#top left on r markdown doc, "source" or "visual"
+
+#keyboard shortcuts
+#tools  keyboard shortcut help
+#control + shift + m creates pipes
+%>% 
+  
+#line up all lines
+#control + shift + a
+  
+#create new command chunk
+# control + alt + i
+  
+# control shift c - use when highlight code and want to assign # to
+  # send to comments
+  
+  #button on top right of script with lines
+#creates outline as heading 
+# or ## 
